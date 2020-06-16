24bf7d01cb5a1aaad2ef59d157ea273a2d13cf58
Valentyn_Skrypnyk <vskrypnyk@ip-192-168-0-101.ec2.internal>
2020-06-16 09:08:22 +0300

    Fix Controller

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,7 @@
 class MainController < ApplicationController
 
   def index
-    @hello = 'PPlease click button \'Run\''
+    @hello = 'PPPlease click button \'Run\''
     @users = User.first(10)
   end
 end
