5070df39704bf44e208f11bb31aaac8e60dee84c
Valentyn_Skrypnyk <vskrypnyk@ip-192-168-0-101.ec2.internal>
2020-06-16 12:48:33 +0300

    Fix Controller

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,7 @@
 class MainController < ApplicationController
 
   def index
-    @hello = 'Qlease click button \'Run\''
+    @hello = 'QWlease click button \'Run\''
     @users = User.first(10)
   end
 end
