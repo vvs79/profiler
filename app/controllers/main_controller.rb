3daaaeea2192ac457a9f15b4395ea3cad0a6252b
Valentyn_Skrypnyk <vskrypnyk@ip-192-168-0-103.ec2.internal>
2020-06-26 11:12:39 +0300

    Fix controller

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,7 +2,7 @@ class MainController < ApplicationController
 
   def index
     RubyProf.start if Rails.env.development?
-    @hello = 'Please click button \'Run\''
+    @hello = 'PPlease click button \'Run\''
     @users = User.last(10)
     if Rails.env.development?
       result = RubyProf.stop
